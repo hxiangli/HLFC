bca442e4ef9b6b1f83272c30c9093722b11da262
hxiangli <hxiangli@qq.com>
2019-04-29 14:20:59 +0800

    spring security ajax

---
+++ src/main/java/com/hlfc/nio/netty/server/EchoServerHandler.java
@@ -0,0 +1,61 @@
+package com.hlfc.nio.netty.server;
+ 
+import java.io.UnsupportedEncodingException;
+
+import com.hlfc.nio.netty.encode.Header;
+import com.hlfc.nio.netty.encode.Message;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+ 
+/**
+ * Handler implementation for the echo server.
+ * 
+ */
+@Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+
+    //客户端连接服务器后调用，简单的就ctx.writeAndFlush(ByteBuf)，复杂点就自定义编解码器
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("客户端连接上来了----"+System.nanoTime());
+
+    }
+
+	//接收请求后的处理类
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    	
+    	Message msg1=(Message)msg;
+    	System.out.println(msg1.getData());
+    	
+    	//此处写接收到客户端请求后的业务逻辑
+    	String content="server say:收到消息了--"+msg1.getData();
+        Header header=new Header((byte)0, (byte)1, (byte)1, (byte)1, (byte)0, "713f17ca614361fb257dc6741332caf2",content.getBytes("UTF-8").length, 1);
+        Message message=new Message(header,content);
+        ctx.writeAndFlush(message);
+        
+        
+    	//ctx.writeAndFlush(Unpooled.copiedBuffer("hello world,this is nettyServer",CharsetUtil.UTF_8));
+    
+    }
+ 
+    //读取完成后处理方法
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        System.out.println("EchoServerHandler.channelReadComplete");
+    	ctx.flush();
+    }
+ 
+    //异常捕获处理方法
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
